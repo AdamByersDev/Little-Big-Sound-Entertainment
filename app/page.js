e41f58990bde3b7e2bc1b9d317459feff120b2dc
Adam Byers <adam@abyers.ca>
2025-05-13 17:30:27 -0400

    Removed unnecessary import

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-import UnderConstruction from "./components/pages/UnderConstruction";
 import HeroSection from "./components/sections/HeroSection";
 /* import PlansSection from "./components/sections/PlansSection"; */
 
