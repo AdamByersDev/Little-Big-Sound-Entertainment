83aae5f34a8dc91fc95afedffb711716cb65f476
Adam Byers <adam@abyers.ca>
2025-05-13 17:26:57 -0400

    Fixed missing import error

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import UnderConstruction from "./components/pages/UnderConstruction";
 import HeroSection from "./components/sections/HeroSection";
-import PlansSection from "./components/sections/PlansSection";
+/* import PlansSection from "./components/sections/PlansSection"; */
 
 export default function Home() {
   return (
