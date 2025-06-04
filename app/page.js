1ff49c12579c6f9130ef4b905b1e422623cf7af3
Adam Byers <adam@abyers.ca>
2025-06-04 16:47:44 -0400

    Added revalidation to the home page

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@ import ContactSection from "./sections/ContactSection";
 import HeroSection from "./sections/HeroSection";
 import PlansSection from "./sections/PlansSection";
 
+export const revalidate = 120;
+
 export default function Home() {
   return (
     <>
